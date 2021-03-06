f51091ad85001d68dbbd1cd56b7e558100905de1
Vodo <andrei.vod@gmail.com>
2021-03-06 14:34:54 +0200

    structure

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Navbar = () => {
+    return(
+        <nav className='nav'>
+            <img src='https://cdn.logo.com/hotlink-ok/logo-social.png'/>
+            <div><a>Profile</a></div>
+            <div><a>Messages</a></div>
+            <div><a>News</a></div>
+            <div><a>Music</a></div>
+            <div><a>Setings</a></div>
+        </nav>
+    )
+}
+
+export default Navbar;

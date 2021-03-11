50953fe0b4c67df1032225b2c1b2ede0dce307f9
AVodo <andrei.vod@gmail.com>
2021-03-11 21:41:01 +0200

    light update

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -3,8 +3,19 @@ import style from './Dialogs.module.css'
 
 const Dialogs = (props) => {
     return (
-        <div>
-            Dialogs
+        <div className={style.dialogsWindow}>
+            <div className={style.dialogItems}>
+                <div className={style.dialog}>Vova</div>
+                <div className={style.dialog}>Andr</div>
+                <div className={style.dialog}>Kate</div>
+                <div className={style.dialog}>Max</div>
+            </div>
+
+            <div className={style.messages}>
+                <div className={style.message}>hi</div>
+                <div className={style.message}>hello</div>
+                <div className={style.message}>loh</div>
+            </div>
         </div>
     )
 }
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,18 +1,33 @@
-import React from "react";
+import React from "react"
 import style from './Navbar.module.css'
-import {NavLink} from "react-router-dom";
+import {NavLink} from "react-router-dom"
 
 const Navbar = () => {
-    return(
+    return (
         <nav className={style.nav}>
-            <img src='https://cdn.logo.com/hotlink-ok/logo-social.png' />
-            <div className={style.item}><NavLink to='/profile'>Profile</NavLink></div>
-            <div className={style.item}><NavLink to='/dialogs'>Messages</NavLink></div>
-            <div className={style.item}><NavLink to='/news'>News</NavLink></div>
-            <div className={style.item}><NavLink to='/music'>Music</NavLink></div>
-            <div className={style.item}><NavLink to='/settings'>Setings</NavLink></div>
+            <img src='https://cdn.logo.com/hotlink-ok/logo-social.png'/>
+
+            <div className={style.item}>
+                <NavLink to='/profile' activeClassName={style.active}>Profile</NavLink>
+            </div>
+
+            <div className={style.item}>
+                <NavLink to='/dialogs' activeClassName={style.active}>Messages</NavLink>
+            </div>
+
+            <div className={style.item}>
+                <NavLink to='/news' activeClassName={style.active}>News</NavLink>
+            </div>
+
+            <div className={style.item}>
+                <NavLink to='/music' activeClassName={style.active}>Music</NavLink>
+            </div>
+
+            <div className={style.item}>
+                <NavLink to='/settings' activeClassName={style.active}>Setings</NavLink>
+            </div>
         </nav>
     )
 }
 
-export default Navbar;
+export default Navbar
